f456171616e30cc79ac22c9c2b164088e8ff4cfe
manci ayman <manciayman0@gmail.com>
2025-07-28 23:38:22 +0300

    add test controller

---
+++ src/main/java/com/task/controller/GreetingsController.java
@@ -0,0 +1,16 @@
+package com.task.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/greetings")
+public class GreetingsController {
+
+    @GetMapping
+    public ResponseEntity<String> sayHello() {
+        return ResponseEntity.ok("Hello from spring boot security");
+    }
+}
