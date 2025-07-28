74c7ea992c7fc0841b9b3e2c77e05122a7c4df06
manci ayman <manciayman0@gmail.com>
2025-07-28 23:36:36 +0300

    add config and jwt files

---
+++ src/main/java/com/task/service/JwtService.java
@@ -0,0 +1,76 @@
+package com.task.service;
+
+import com.task.entity.User;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+@Service
+public class JwtService {
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+    @Value("${jwt.expiration}")
+    private long jwtExpirationMs;
+
+    @Value("${jwt.refreshExpiration}")
+    private long refreshExpirationMs;
+
+    SecretKey key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+
+    public String extractUserEmail(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser().setSigningKey(key).parseClaimsJws(token).getBody();
+    }
+
+    public String generateToken(User user) {
+        Map<String, Object> claims = new HashMap<>();
+        return buildToken(claims, user, jwtExpirationMs);
+    }
+
+    public String generateRefreshToken(User user) {
+        return buildToken(new HashMap<>(), user, refreshExpirationMs);
+    }
+
+    private String buildToken(Map<String, Object> claims, User user, long expiration) {
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(user.getEmail())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + expiration))
+                .claim("authorities", user.getAuthorities())
+                .signWith(SignatureAlgorithm.HS256, key)
+                .compact();
+    }
+
+    public Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    private Boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    public Boolean isTokenValid(String token, User user) {
+        final String email = extractUserEmail(token);
+        return (email.equals(user.getEmail()) && !isTokenExpired(token));
+    }
+}
